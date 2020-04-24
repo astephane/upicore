da00153ac82b0226074599e27e6a5253a11ec945
Stéphane ALBERT <stephane.albert.fr@gmail.com>
2020-04-24 17:31:51 +0200

    Split-out cxx::memory.

---
+++ src/cxx/memory.hpp
@@ -0,0 +1,31 @@
+//
+// This file is part of upicore.
+//
+// upicore is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// upicore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with upicore.  If not, see <https://www.gnu.org/licenses/>.
+//
+#ifndef CXX_MEMORY_HPP
+#define CXX_MEMORY_HPP
+
+
+#include <type_traits>
+
+
+namespace cxx
+{
+  template< typename T >
+  using raw_ptr = std::add_pointer_t< T >;
+
+} // end of namespace 'cxx'.
+
+#endif // CXX_MEMORY_HPP
