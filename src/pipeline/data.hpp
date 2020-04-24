602af95d2c94235fe98bce107736dc4af80cd6fc
Stéphane ALBERT <stephane.albert.fr@gmail.com>
2020-04-24 17:32:28 +0200

    Split-out pipeline::data.

---
+++ src/pipeline/data.hpp
@@ -0,0 +1,38 @@
+//
+// This file is part of upicore.
+//
+// upicore is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// upicore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with upicore.  If not, see <https://www.gnu.org/licenses/>.
+//
+#ifndef PIPELINE_DATA_HPP
+#define PIPELINE_DATA_HPP
+
+
+// #include <cassert>
+// #include <iostream>
+// #include <memory>
+// #include <stdexcept>
+// #include <tuple>
+// #include <type_traits>
+
+
+namespace pipeline
+{
+
+  struct data
+  {
+  };
+
+} // end of namespace 'pipeline'.
+
+#endif // PIPELINE_DATA_HPP
