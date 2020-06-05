d250b73eb029764f294ec7bf213ef299ebe91cc1
Stéphane ALBERT <stephane.albert.fr@gmail.com>
2020-06-05 16:18:27 +0200

    Added trace.hpp/cpp.

---
+++ src/cxx/trace.hpp
@@ -0,0 +1,36 @@
+//
+// This file is part of upicore.
+//
+// upicore is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// upicore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with upicore.  If not, see <https://www.gnu.org/licenses/>.
+//
+#ifndef CXX_TRACE_HPP
+#define CXX_TRACE_HPP
+
+
+#define TRACE_FUN_IMPL( p )			\
+  std::cout					\
+  << ( p )					\
+  << " " << __PRETTY_FUNCTION__			\
+  << std::endl
+
+#define TRACE_THIS_FUN() TRACE_FUN_IMPL( this )
+
+// #define TRACE_FUN() TRACE_FUN_IMPL( nullptr )
+
+
+namespace cxx
+{
+} // end of namespace 'cxx'.
+
+#endif // CXX_TRACE_HPP
