599a0f76170cc7d8f3004347da570d4ccc4278f8
Stéphane ALBERT <stephane.albert.fr@gmail.com>
2020-08-07 07:47:28 +0200

    Added cxx::non_copyable

---
+++ src/cxx/non_copyable.hpp
@@ -0,0 +1,33 @@
+//
+// This file is part of upicore.
+//
+// upicore is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// upicore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with upicore.  If not, see <https://www.gnu.org/licenses/>.
+//
+#ifndef CXX_NON_COPYABLE_HPP
+#define CXX_NON_COPYABLE_HPP
+
+
+namespace cxx
+{
+
+struct non_copyable
+{
+  non_copyable( non_copyable const & ) = delete;
+  non_copyable & operator = ( non_copyable const & ) = delete;
+};
+
+} // namespace cxx.
+
+
+#endif // CXX_NON_COPYABLE_HPP
